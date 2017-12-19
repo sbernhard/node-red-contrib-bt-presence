07876f53c168211a4d8de1f4d403e02df868de7e
Bernhard Suttner <b.suttner@gmail.com>
2017-12-19 09:46:26 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,25 @@
+# node-red-contrib-bt-presence
+
+A <a href="http://nodered.org" target="_new">Node-RED</a> node which uses l2ping to determine the presence of one or more bluetooth devices. This node
+does provide the collective bluetooth state. Means, only if no bluetooth device at all is reachable, "not-present" will be signalized. 
+
+The node uses https://github.com/cmvee/bt-presence
+
+## Install
+
+Run the following command in the root directory of your Node-RED install or use the "Manage palette"
+
+    npm install node-red-contrib-bt-presence
+
+Currently (2017-12-19) a patch for bt-presence is necessary: https://github.com/cmvee/bt-presence/pull/3
+Make sure that this change is used. You will need to patch it manually right now. 
+
+## Usage
+
+* Add the bluetooth devices you want to scan. To enter more than one, use a comma to separate them. IMPORTANT: no space between comma and previous/next device
+* Set the scan interval.
+* Deploy your flow 
+* "present" will be signalized in msg.payload in case of presence. Otherwise "not-present"
+
+## Thanks
+Thanks to "cmvee" for the npm modul "bt-presence" which this node-red node uses.
---
+++ bt-presence/bt-presence.js
@@ -0,0 +1,92 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+
+	"use strict";
+
+  function BtPresenceNode(config) {
+
+		RED.nodes.createNode(this, config);
+		var node = this;
+    var btPresence = require('bt-presence')
+
+    node.log("Initialize btPresence");
+
+    function sendPresent() {
+      node.log("BT scan result: present");
+
+      const msg = {
+        title: "BT present",
+        description: "At least one BT device is available",
+        payload: "present"
+      };
+      node.send(msg)
+    }
+
+    function sendNotPresent() {
+      node.log("BT scan result: not-present");
+
+      const msg = {
+        title: "BT not present",
+        description: "No BT device of your devices list is available",
+        payload: "not-present"
+      };
+      node.send(msg)
+    }
+
+    var interval = 60;
+    if (config.interval !== undefined) {
+      if (Number.isInteger(parseInt(config.interval))) {
+        interval = config.interval;
+      }
+    } else {
+      node.warn("Interval is not set / is not a number. Use default: 60");
+    }
+
+    var devices = [];
+    if (config.devices !== undefined) {
+      devices = config.devices.split(",").map(function(item) {
+        return item.trim();
+      });
+    }
+
+		if (devices.length < 1) {
+			node.error('No devices are specificed. Please specify the devices in node configuration!');
+		} else {
+
+      btPresence.setDevices(devices);
+      btPresence.setScanInterval(interval);
+
+      // Looks like the btPresence Object does survive. Remove the
+      // Listeners before we add the new one
+      btPresence.removeAllListeners();
+      btPresence.on('present', sendPresent);
+      btPresence.on('not-present', sendNotPresent);
+
+      node.log("Starting bluetooth scan for "+ devices.length + " devices every "+ interval + " seconds!");
+      btPresence.start(true);
+
+      // On Close, stop the presence scan
+      node.on('close', function () {
+        node.log("Close bt-presence node");
+        btPresence.stop()
+      });
+    }
+  }
+
+  RED.nodes.registerType("bt-presence", BtPresenceNode);
+}
+
+// vim: ts=2 sw=2 sts=2 et
